1a96a7c351b1d308204b4d2c2e052a1f0eb0d40c
bpronin <boris.i.pronin@gmail.com>
2021-05-19 21:46:30 +0300

    version 1.0.1

--- src/tracks-to-markers.py
+++ src/tracks-to-markers.py
@@ -1,60 +1,90 @@
+# Converts track list into Adobe Audition's markers file
+# Source file must be formatted as follows:
+# ---
+# TOTAL DURATION {[HH:]MM:SS}[,TIME FACTOR {FLOAT}]
+# TRACK START TIME {[HH:]MM:SS},TRACK NAME {STRING}
+# ...
+# ---
+
+import sys
 import ntpath
 import datetime
 
-SRC_FILENAME = "D:/Temp/Perfect Love Songs/tracks"
-TIME_FACTOR = 1.003
+
+class Track:
+    pass
 
 
-def parse_duration(s):
-    t = datetime.datetime.strptime(s, "%H:%M:%S")
-    return datetime.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second) * TIME_FACTOR
+def parse_time(s, factor):
+    fmt = "%M:%S" if s.count(":") == 1 else "%H:%M:%S"
+    time = datetime.datetime.strptime(s, fmt)
+    return datetime.timedelta(hours=time.hour, minutes=time.minute, seconds=time.second) * factor
 
 
-def format_duration(t):
-    seconds = t.seconds
+def format_time(t):
+    seconds = abs(int(t.seconds))
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
     return "%d:%02d:%02d.000" % (hours, minutes, seconds)
 
 
-def write_line(file, no, start, dur, name):
-    ln = "%02d - %s\t%s\t%s\tdecimal\tCue\t\n" % (no, name, format_duration(start), format_duration(dur))
-    file.write(ln)
+def format_dst_filename(src):
+    fn = ntpath.basename(src).split(".")[0] + ".csv"
+    return ntpath.join(ntpath.dirname(src), "markers-" + fn)
 
 
-dst_filename = ntpath.join(ntpath.dirname(SRC_FILENAME), "markers.csv")
-src_file = open(SRC_FILENAME, "r")
-dst_file = open(dst_filename, "w")
+def read_tracks(file):
+    field_separator = ","
+    line = file.readline().rstrip("\n")
+    fields = line.split(field_separator)
+    time_factor = float(fields[1]) if len(fields) > 1 else 1.0
+    total_duration = parse_time(fields[0], time_factor)
 
-
-def parse_track_no(s):
-    return int(s.split(".")[0])
-
-
-try:
-    dst_file.write("Name\tStart\tDuration\tTime Format\tType\tDescription\n")
-
-    line = src_file.readline().strip()
-    fields = line.split("\t")
-    track_name = fields[2]
-    start_time = parse_duration(fields[1])
-    track_no = 1
-
+    tracks = []
     while True:
-        line = src_file.readline().strip()
-        if not line:
+        line = file.readline().rstrip("\n")
+        if line == "":
             break
+        fields = line.split(field_separator)
 
-        fields = line.split("\t")
-        end_time = parse_duration(fields[1])
+        track = Track()
+        track.start = parse_time(fields[0], time_factor)
+        track.name = fields[1]
 
-        write_line(dst_file, track_no, start_time, end_time - start_time, track_name)
+        tracks.append(track)
+    return tracks, total_duration
 
-        track_name = fields[2]
-        start_time = end_time
-        track_no += 1
 
-    write_line(dst_file, track_no, start_time, datetime.timedelta(seconds=10), track_name)
-finally:
-    src_file.close()
-    dst_file.close()
+def update_tracks(tracks, total_duration):
+    i = 0
+    n = len(tracks) - 1
+    while i <= n:
+        track = tracks[i]
+        end = tracks[i + 1].start if i < n else total_duration
+        track.duration = end - track.start
+        i += 1
+
+
+def write_tracks(tracks, file):
+    file.write("Name\tStart\tDuration\tTime Format\tType\tDescription\n")
+    for i, track in enumerate(tracks):
+        line = "%02d - %s\t%s\t%s\tdecimal\tCue\t" % (i + 1,
+                                                      track.name,
+                                                      format_time(track.start),
+                                                      format_time(track.duration))
+        file.write(line + "\n")
+
+
+def main():
+    src_filename = str(sys.argv[1])
+    with open(src_filename, "r") as file:
+        tracks, total_duration = read_tracks(file)
+
+    update_tracks(tracks, total_duration)
+
+    dst_filename = format_dst_filename(src_filename)
+    with open(dst_filename, "w") as file:
+        write_tracks(tracks, file)
+
+
+main()
